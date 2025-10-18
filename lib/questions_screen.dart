e367868efa9691259a481157414b9557b3f4045d
huseyinozenalbayrak <albayrakhuseyinozen@gmail.com>
2025-10-18 16:34:39 +0300

    feat: implement questions screen with answer buttons and question data

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +14,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Questions Screen!'));
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
+          SizedBox(height: 30),
+
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ],
+      ),
+    );
   }
 }
