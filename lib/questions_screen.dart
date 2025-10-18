476a244384eb8cf0473016d7bfffbceac6b4a132
huseyinozenalbayrak <albayrakhuseyinozen@gmail.com>
2025-10-18 16:40:05 +0300

    refactor: improve layout structure and alignment in questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,19 +18,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: TextStyle(color: Colors.white, fontSize: 14),
-          ),
-          SizedBox(height: 30),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
 
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-        ],
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
